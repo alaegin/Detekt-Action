4cdbe4fbb4e47f4a40bb75001902ce7d4d40b7ac
Alexey Egin <egin9@Mail.ru>
2020-05-28 23:39:05 +0300

    Move detekt excludes to inputs in action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ cd "$GITHUB_WORKSPACE" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-detekt --fail-fast --config "${INPUT_DETEKT_CONFIG}" --report xml:detekt_report.xml --excludes '**/build/**,**/.idea/**' \
+detekt --fail-fast --config "${INPUT_DETEKT_CONFIG}" \
+  --report xml:detekt_report.xml \
+  --excludes "${INPUT_DETEKT_EXCLUDES}" \
   --plugins /opt/detekt-formatting-1.9.1.jar
-reviewdog -f=checkstyle -name="detekt" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" -filter-mode=nofilter < detekt_report.xml
+
+reviewdog -f=checkstyle -name="detekt" -reporter="${INPUT_REPORTER}" \
+  -level="${INPUT_LEVEL}" -filter-mode=nofilter <detekt_report.xml
