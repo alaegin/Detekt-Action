0e763726016f90c926ace450248b5f4d28270233
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-19 19:42:34 +0200

    Update detekt to v1.10.0-RC1 (#4)
    * Update detekt to v1.10.0-RC1
    
    * update action.yml
    
    * Fix error in dockerfile
    
    * Remove fork change

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 detekt --fail-fast --config "${INPUT_DETEKT_CONFIG}" \
   --report xml:detekt_report.xml \
   --excludes "${INPUT_DETEKT_EXCLUDES}" \
-  --plugins /opt/detekt-formatting-1.9.1.jar
+  --plugins /opt/detekt-formatting.jar
 
 reviewdog -f=checkstyle -name="detekt" -reporter="${INPUT_REVIEWDOG_REPORTER}" \
   -level="${INPUT_REVIEWDOG_LEVEL}" -filter-mode=nofilter <detekt_report.xml
